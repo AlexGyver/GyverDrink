969e76498aa0153aaa69ddffdf37ddb5d815e068
Victor Glekler <victor.glekler@gmail.com>
2020-03-25 23:09:38 +0100

    upd

--- firmware/GyverDrink_v1.5_StepMot/c_func.ino
+++ firmware/GyverDrink_v1.5_StepMot/c_func.ino
@@ -127,7 +127,9 @@ void flowRoutnie() {
         curPumping = i;                                   // запоминаем выбор
         systemState = MOVING;                             // режим - движение
         shotStates[curPumping] = IN_PROCESS;              // стакан в режиме заполнения
+        #ifndef STEPPER_AUTO_POWER
         stepper.enable();
+        #endif
         stepper.setAngle(shotPos[curPumping]);            // задаём цель
         DEBUG("find glass");
         DEBUG(curPumping);
