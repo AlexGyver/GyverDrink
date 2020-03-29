07460f8d785056b5da46f8dc0e07295f8f97b1bb
Victor Glekler <victor.glekler@gmail.com>
2020-03-29 04:14:36 +0200

    Update GyverDrink_v1.5_StepMot.ino

--- firmware/GyverDrink_v1.5_StepMot/GyverDrink_v1.5_StepMot.ino
+++ firmware/GyverDrink_v1.5_StepMot/GyverDrink_v1.5_StepMot.ino
@@ -41,10 +41,10 @@
 #define SWITCH_LEVEL 0                    // кнопки 1 - высокий сигнал при замыкании, 0 - низкий
 #define STEPS_PER_REVOLUTION 2037.88642   // количество шагов на оборот двигателя
 #define STEPPER_ENDSTOP_INVERT  0         // 1 - высокий сигнал при замыкании, 0 - низкий
-#define STEPPER_POWERSAFE OFF             // автоматическое управление питанием шагового двигателя (питание включается только при движении)
-#define INVERT_STEPPER 0                  // инвертировать направление вращения шагового двигателя
+#define STEPPER_POWERSAFE 0               // автоматическое управление питанием шагового двигателя (питание включается только при движении)
+#define INVERT_STEPPER 1                  // инвертировать направление вращения шагового двигателя
 #define STEPPER_SPEED 15                  // скорость двигателя в оборотах в минуту
-#define MICROSTEPS  1                     // значение микрошага, выставленного на драйвере двигателя
+#define MICROSTEPS  2                     // значение микрошага, выставленного на драйвере двигателя
 
 
 // положение крана над центрами рюмок в градусах от нулевой точки
@@ -58,44 +58,44 @@ const long time50ml = 5500;
 #define KEEP_POWER OFF    // ON - система поддержания питания ПБ, чтобы он не спал
 
 // отладка
-#define DEBUG_UART OFF
+#define DEBUG_UART ON
 
 // =========== ПИНЫ Arduino Nano===========
-#define PUMP_POWER 3
-#define STEPPER_STEP  5
-#define STEPPER_DIR 4
-#define STEPPER_EN  2
-#define STEPPER_ENDSTOP 13
-#define LED_PIN 6
-#define BTN_PIN 7
-#define ENC_SW 8
-#define ENC_DT 9
-#define ENC_CLK 10
-#define DISP_DIO 11
-#define DISP_CLK 12
-const byte SW_pins[] = {A0, A1, A2, A3, A4, A5};
+//#define PUMP_POWER 3
+//#define STEPPER_STEP  5
+//#define STEPPER_DIR 4
+//#define STEPPER_EN  2
+//#define STEPPER_ENDSTOP 13
+//#define LED_PIN 6
+//#define BTN_PIN 7
+//#define ENC_SW 8
+//#define ENC_DT 9
+//#define ENC_CLK 10
+//#define DISP_DIO 11
+//#define DISP_CLK 12
+//const byte SW_pins[] = {A0, A1, A2, A3, A4, A5};
 
 // =========== ПИНЫ Arduino Micro===========
-//#define PUMP_POWER      0
-//#define BTN_PIN         1
-//#define DISP_DIO        2
-//#define DISP_CLK        3
-//#define BAT_PIN         4
-//#define LED_PIN         5
-//#define STEPPER_STEP    6
-//#define STEPPER_DIR     7
-//#define STEPPER_EN      8
+#define PUMP_POWER      0
+#define BTN_PIN         1
+#define DISP_DIO        2
+#define DISP_CLK        3
+#define BAT_PIN         4
+#define LED_PIN         5
+#define STEPPER_STEP    6
+#define STEPPER_DIR     7
+#define STEPPER_EN      8
 //#define STEPPER_ENDSTOP 9
-//#define ENC_SW          10
-//#define ENC_DT          14
-//#define ENC_CLK         16
-//const byte SW_pins[] = {15, 18, 19, 20, 21};
+#define ENC_SW          10
+#define ENC_DT          14
+#define ENC_CLK         16
+const byte SW_pins[] = {15, 18, 19, 20, 21};
 
 // =========== ЛИБЫ ===========
 #include <GyverTM1637.h>
 #include <microLED.h>
 #include <EEPROM.h>
-#include "StepMot.h"
+#include <StepMot.h>
 #include "encUniversalMinim.h"
 #include "buttonMinim.h"
 #include "timer2Minim.h"
@@ -123,7 +123,7 @@ int8_t curPumping = -1;
 
 enum {NO_GLASS, EMPTY, IN_PROCESS, READY} shotStates[NUM_SHOTS];
 enum {SEARCH, MOVING, WAIT, PUMPING} systemState;
-bool workMode = 0;  // 0 manual, 1 auto
+bool workMode = 1;  // 0 manual, 1 auto
 uint8_t thisVolume = 50;
 bool systemON = false;
 bool timeoutState = false;
@@ -134,7 +134,7 @@ bool parking = false;
 #define pumpON() digitalWrite(PUMP_POWER, 1)
 #define pumpOFF() digitalWrite(PUMP_POWER, 0)
 
-#if (DEBUG_UART == 1)
+#if (DEBUG_UART == ON)
 #define DEBUG(x) Serial.println(x)
 #else
 #define DEBUG(x)
